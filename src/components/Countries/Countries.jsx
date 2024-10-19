1ba6db4f5ee96e456bf9d85eb7120d39a7b8c6a1
kobirul5 <kobirul05j@gmail.com>
2024-10-20 01:21:47 +0600

    added all country and fetch it, and show it on disply

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+import { useState } from 'react'
+import Country from '../Country/Country';
+import './Countries.css'
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    const [visitedCountry, setVisitedCountry] = useState([])
+
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
+
+    const handelVisitedCountry = country => {
+        console.log(country)
+        const newVisitedCountry = [...visitedCountry, country]
+        setVisitedCountry(newVisitedCountry)
+        
+    }
+
+    return (
+        <div>
+            <h1>Countries:- {countries.length}</h1>
+            <div>
+                <h3>Visited Country:- {visitedCountry.length}</h3>
+                <ul>
+                    {visitedCountry.map(country=> <li key={country.cca3}>{country.name.common}</li>)}
+                </ul>
+            </div>
+            <div className='country-container'>
+                {countries.map(country => <Country 
+                key={country.cca3} 
+                handelVisitedCountry = {handelVisitedCountry}
+                country={country}></Country>)}
+            </div>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './Country.css'
+const Country = ({country, handelVisitedCountry}) => {
+    const {name, flags, population, area, cca3} = country
+    const [visited, setVisited] = useState(false)
+    const handelVisitedBtn = () => {
+        setVisited(!visited)
+    }
+
+    return (
+        <div className={`country ${visited && 'visited'}`}>
+            <h2 style={{color: visited? 'purple': 'black'}}>Name:- {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Population:- {population}</p>
+            <p>Area:- {area}</p>
+            <p>Code:- {cca3}</p>
+            <button onClick={()=>handelVisitedCountry(country)}>Mark as visited</button><br/> <br/>
+            <button onClick={handelVisitedBtn}>{visited? 'Visited': 'Going'}</button>
+            <p>{visited? 'I have visited this country' : 'I want to go this country'}</p>
+        </div>
+    );
+};
+
+export default Country;
