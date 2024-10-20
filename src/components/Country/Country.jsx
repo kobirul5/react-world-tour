34472933ee9b0cb06c1c50955d1df5f8d7ebfc38
kobirul5 <kobirul05j@gmail.com>
2024-10-20 10:59:27 +0600

    git push

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -15,7 +15,7 @@ const Country = ({country, handelVisitedCountry, handelVisitedCountryFlags}) => 
             <p>Area:- {area}</p>
             <p>Code:- {cca3}</p>
             <button onClick={()=>handelVisitedCountry(country)}>Mark as visited</button><br/> <br/>
-            <button onClick={()=>handelVisitedCountryFlags(country.flags.png)}>Add Flag</button>
+            <button onClick={()=>handelVisitedCountryFlags(country.flags.png)}>Add Flag</button> <br /> <br />
             <button onClick={handelVisitedBtn}>{visited? 'Visited': 'Going'}</button>
             <p>{visited? 'I have visited this country' : 'I want to go this country'}</p>
         </div>
